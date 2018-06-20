e0395d0f93994168c4916f496037877cb6a6458e
Daniel Augusto de Melo Santos <danielmelos@gmail.com>
2018-06-20 19:37:04 -0300

    Testing CircleCI status

--- src/testModule.js
+++ src/testModule.js
@@ -1,10 +1,9 @@
+/*eslint-env node*/
 function testingJestCoverage (param1, param2) {
-    if (param1 == '10' || param2 == '21') {
-        const a = param1;
-        const b = param2;
-        return a + b;
+    if (param1 === '10' || param2 === '21') {
+        return param1 + param2;
     }
     return 30;
-};
+}
 
 module.exports = testingJestCoverage;
